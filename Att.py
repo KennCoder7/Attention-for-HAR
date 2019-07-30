d7f958d40b64f478e92d427420041b9fdf1721fc
Kenn <40631472+KennCoder7@users.noreply.github.com>
2019-07-30 09:17:43 +0800

    Update Att.py

--- Att.py
+++ Att.py
@@ -3,10 +3,10 @@ from kenn.utils import load_data
 
 
 def main():
-    train_x, train_y, test_x, test_y = load_data('UCI')
+    train_x, train_y, test_x, test_y = load_data('UCI_3axis')
     model = AttCNN(train_x, train_y, test_x, test_y,
                    seg_len=128, num_channels=3, num_labels=6,
-                   num_conv_for_extract=2, filters=16, k_size=5, conv_strides=1, pool_size=2, pool_strides=2,
+                   num_conv_for_extract=3, filters=16, k_size=5, conv_strides=1, pool_size=2, pool_strides=2,
                    batch_size=100, learning_rate=0.0001, num_epochs=1000,
                    print_val_each_epoch=2, print_test_each_epoch=10, print_test=True,
                    cpt_func='dot', norm_func='softmax', padding='same',
